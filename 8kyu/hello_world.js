45a4c9897b4b5c1d5e7a09fe610ab5d0a052514f
skimasan <skimasan@gmail.com>
2025-01-16 23:26:11 +0500

    hello_world.js - 2

--- 8kyu/hello_world.js
+++ 8kyu/hello_world.js
@@ -2,10 +2,16 @@
 //
 //     Make a simple function called greet that returns the most-famous "hello world!".
 
-//SOLUTION 1 simple:
+// SOLUTION 1 simple:
+
+// function greet() {
+//     return "hello world!";
+// }
+
+// SOLUTION 2 creative:
 
 function greet() {
-    return "hello world!";
+    return String.fromCharCode(104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 33);
 }
 
 console.log(greet());
