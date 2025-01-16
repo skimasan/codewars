1234fa0042ff78340c18c855bf5cadf46f4eb6c2
skimasan <skimasan@gmail.com>
2025-01-16 23:25:28 +0500

    hello_world.js

---
+++ 8kyu/hello_world.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+//
+//     Make a simple function called greet that returns the most-famous "hello world!".
+
+//SOLUTION 1 simple:
+
+function greet() {
+    return "hello world!";
+}
+
+console.log(greet());
