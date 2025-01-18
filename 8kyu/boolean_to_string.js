0493b46eb15d401f38aaf15dde4340671c8ef694
skimasan <skimasan@gmail.com>
2025-01-18 14:26:27 +0500

    boolean_to_string.js - 2

--- 8kyu/boolean_to_string.js
+++ 8kyu/boolean_to_string.js
@@ -3,8 +3,13 @@
 //     Note: Only valid inputs will be given.
 
 //SOLUTION 1:
+// function booleanToString(b){
+//     return b + "";
+// }
+
+// SOLUTION 2:
 function booleanToString(b){
-    return b + "";
+    return String(b);
 }
 
 console.log(booleanToString(true))
