81422ec36d53450cac9876d2a1a771a7bacdfb86
skimasan <skimasan@gmail.com>
2025-01-18 14:24:16 +0500

    boolean_to_string.js

---
+++ 8kyu/boolean_to_string.js
@@ -0,0 +1,10 @@
+// Implement a function which convert the given boolean value into its string representation.
+//
+//     Note: Only valid inputs will be given.
+
+//SOLUTION 1:
+function booleanToString(b){
+    return b + "";
+}
+
+console.log(booleanToString(true))
